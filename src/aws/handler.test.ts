aff67e339f711d44773b19ead95157aa921c0203
emanuelherrmann <emanuelherrmann@gmail.com>
2018-10-26 06:17:15 +0000

    fix test

--- src/aws/handler.test.ts
+++ src/aws/handler.test.ts
@@ -12,7 +12,7 @@ describe('aws/handler', () => {
             const event = createEvent({
                 template: 'aws:scheduled',
                 merge: {
-                  instances: [''],
+                  instances: ['i-0817c3239d7db3d89'],
                   operation: 'STOP',
                   region: 'us-west-2',
                 },
